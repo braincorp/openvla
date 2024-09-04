c7348836ade66904518f9b4fae525cb8a90a76f4
Jonathon Sather <jonathon.sather@braincorp.com>
2024-09-04 10:20:54 -0700

    working on validation

--- vla-scripts/validate_finetuned_policy.py
+++ vla-scripts/validate_finetuned_policy.py
@@ -3,6 +3,7 @@ import json
 import os
 
 import torch
+from PIL import Image
 from peft import prepare_model_for_kbit_training
 from torch.utils.data import DataLoader
 from transformers import AutoProcessor, AutoModelForVision2Seq, BitsAndBytesConfig, AutoConfig, AutoImageProcessor
@@ -15,6 +16,7 @@ from prismatic.models.backbones.llm.prompting import PurePromptBuilder
 from prismatic.util.data_utils import PaddedCollatorForActionPrediction
 from prismatic.vla.action_tokenizer import ActionTokenizer
 from prismatic.vla.datasets import RLDSBatchTransform, RLDSDataset
+import numpy as np
 
 ATTENTION_IMPLEMENTATION_NAME = "flash_attention_2"
 DEFAULT_DEVICE_NAME = "cuda:0"
@@ -32,15 +34,81 @@ def get_datasets_dir_path() -> str:
     return os.path.expanduser('~/brawn_artifacts/datasets/widowx_250s/episodes_pick_mustard')
 
 
+def patch_predict_action_tokens(vla: AutoModelForVision2Seq) -> None:
+    @torch.inference_mode()
+    def predict_action_tokens(
+            image: Image,
+            instruction: str,
+            unnorm_key: Optional[str] = None,
+            **kwargs: str
+    ) -> np.ndarray:
+        """
+        Monkey patch for OpenVLA class.
+        Core function for VLA inference; maps input image and task instruction to action tokens.
+
+        @param image: PIL Image as [height, width, 3]
+        @param instruction: Task instruction string
+        @param unnorm_key: Optional dataset name for retrieving un-normalizing statistics; if None, checks that model
+            was trained only on a single dataset, and retrieves those statistics.
+        @return action tokens
+        """
+        image_transform, tokenizer = vla.vision_backbone.image_transform, vla.llm_backbone.tokenizer
+
+        # Build VLA Prompt
+        prompt_builder = vla.get_prompt_builder()
+        prompt_builder.add_turn(role="human", message=f"What action should the robot take to {instruction.lower()}?")
+        prompt_text = prompt_builder.get_prompt()
+
+        # Prepare Inputs
+        input_ids = tokenizer(prompt_text, truncation=True, return_tensors="pt").input_ids.to(vla.device)
+        if isinstance(tokenizer, LlamaTokenizerFast):
+            # Note: We need to add this special empty token ('') after the colon (':') token in "ASSISTANT:"
+            #       in order for the predictions to match the training configuration and be accurate.
+            input_ids = torch.cat(
+                (input_ids, torch.unsqueeze(torch.Tensor([29871]).long(), dim=0).to(vla.device)), dim = 1
+            )
+        else:
+            raise ValueError(f"Unsupported `tokenizer` type = {type(tokenizer)}")
+
+        # Preprocess Image
+        pixel_values = image_transform(image)
+        if isinstance(pixel_values, torch.Tensor):
+            pixel_values = pixel_values[None, ...].to(vla.device)
+        elif isinstance(pixel_values, dict):
+            pixel_values = {k: v[None, ...].to(vla.device) for k, v in pixel_values.items()}
+        else:
+            raise ValueError(f"Unsupported `pixel_values` type = {type(pixel_values)}")
+
+        # Invoke super().generate --> taps into `GenerationMixin` which (redirects) to `forward()`
+        autocast_dtype = vla.llm_backbone.half_precision_dtype
+        with torch.autocast("cuda", dtype=autocast_dtype, enabled=vla.enable_mixed_precision_training):
+            # fmt: off
+            generated_ids = super(PrismaticVLM, self).generate(
+                input_ids=input_ids,  # Shape: [1, seq]
+                pixel_values=pixel_values,  # Shape: [1, 3, res, res] or Dict[str, ...]
+                max_new_tokens=vla.get_action_dim(unnorm_key),
+                **kwargs
+            )
+            # fmt: on
+        return generated_ids
+
+    vla.predict_action_tokens = predict_action_tokens
+    return vla
+
+
 def _evaluate_model_on_dataset(
         vla: AutoModelForVision2Seq,
         action_tokenizer: ActionTokenizer,
         dataloader: DataLoader,
-        device_name: str
+        device_name: str,
+        processor: AutoProcessor
 ):
     """Helper to evaluate the model on the dataset."""
+    action_norm_stats = vla.get_action_stats('episodes_pick_mustard_rlds')
     accuracies = []
+    accuracies_policy = []
     losses = []
+
     for batch_idx, batch in enumerate(dataloader):
         input_ids = batch["input_ids"].to(device_name)
         attention_mask = batch["attention_mask"].to(device_name)
@@ -64,7 +132,60 @@ def _evaluate_model_on_dataset(
         correct_preds = (action_preds == action_gt) & mask
         action_accuracy = correct_preds.sum().float() / mask.sum().float()
         accuracies.append(action_accuracy.item())
-        print(f"Batch {batch_idx} Action Accuracy: {action_accuracy.item()}, Loss: {loss.item()}")
+
+        # Generate tokens the way it's done in the policy
+        image = np.interp(
+            batch["pixel_values"][0][3:].numpy().transpose(1, 2, 0),
+            (-1, 1),
+            (0, 255)
+        ).astype(np.uint8)
+        instruction = processor.decode(input_ids[0][10:-13], skip_special_tokens=True)
+        image_pil = Image.fromarray(image)
+        network_inputs = processor(
+            f"In: What action should the robot take to {instruction.lower()}?\nOut:",
+            image_pil
+        ).to(device_name, dtype=torch.bfloat16)
+        unnorm_key = 'episodes_pick_mustard_rlds'
+        generated_ids = vla.generate(**network_inputs, max_new_tokens=vla.get_action_dim(unnorm_key))
+
+        action_tokens_training = action_preds.to('cpu').numpy()[0][mask.to('cpu').numpy()[0]]
+        action_tokens_policy = generated_ids[0, -vla.get_action_dim(unnorm_key) :].cpu().numpy()
+        action_tokens_gt = action_gt.to('cpu').numpy()[0][mask.to('cpu').numpy()[0]]
+
+        accuracy_policy = (action_tokens_gt == action_tokens_policy).sum() / len(action_tokens_training)
+        accuracies_policy.append(accuracy_policy)
+        print(f"Batch {batch_idx} Loss: {loss.item()}, Action Accuracy: {action_accuracy.item()}, Action Accuracy (policy): {accuracy_policy}")
+        import pdb; pdb.set_trace()
+
+        action_high, action_low = np.array(action_norm_stats["q99"]), np.array(action_norm_stats["q01"])
+        action_preds_token_ids = action_preds[0, -vla.get_action_dim('episodes_pick_mustard_rlds'):]
+        action_preds_normalized = action_tokenizer.decode_token_ids_to_actions(action_preds_token_ids.cpu().numpy())
+        action_mask = action_norm_stats.get("mask", np.ones_like(action_norm_stats["q01"], dtype=bool))
+        action_preds_unnorm = np.where(
+            action_mask,
+            0.5 * (action_preds_normalized + 1) * (action_high - action_low) + action_low,
+            action_preds_normalized,
+        )
+
+        image = np.interp(
+            batch["pixel_values"][0][3:].numpy().transpose(1, 2, 0),
+            (-1, 1),
+            (0, 255)
+        ).astype(np.uint8)
+        import cv2
+        cv2.imshow('image', image[:, :, ::-1])
+        cv2.waitKey(0)
+
+        instruction = processor.decode(input_ids[0][10:-13], skip_special_tokens=True)
+        print(f'instruction: {instruction}')
+        image_pil = Image.fromarray(image)
+        network_inputs = processor(f"In: What action should the robot take to {instruction.lower()}?\nOut:", image_pil).to(device_name, dtype=torch.bfloat16)
+        import pdb; pdb.set_trace()
+
+        action_policy = vla.predict_action(**network_inputs, unnorm_key='episodes_pick_mustard_rlds')  # from modeling_prismatic.py
+        print(f'action preds: {action_preds_unnorm}')
+        print(f'action policy: {action_policy}')
+        # TODO: Figure out why these disagree... see recent terminal
 
     print(f'Mean accuracy: {sum(accuracies) / len(accuracies)}')
     print(f'Mean loss: {sum(losses) / len(losses)}')
@@ -92,15 +213,6 @@ def validate_finetuned_policy(
         trust_remote_code=True,
     )
 
-    norm_stats_json = os.path.join(checkpoint_path, "dataset_statistics.json")
-    with open(norm_stats_json, "r") as f:
-        norm_stats = json.load(f)
-
-    if len(norm_stats) != 1:
-        raise ValueError(f"Expected one dataset key, got {len(norm_stats)}")
-
-    vla.norm_stats = norm_stats  # hacky
-
     # Load dataset
     action_tokenizer = ActionTokenizer(processor.tokenizer)
     batch_transform = RLDSBatchTransform(
@@ -115,8 +227,8 @@ def validate_finetuned_policy(
         batch_transform=batch_transform,
         resize_resolution=tuple(vla.config.image_sizes),
         shuffle_buffer_size=100,
-        image_aug=False,
-        train=False
+        image_aug=True,
+        train=True
     )
 
     collator = PaddedCollatorForActionPrediction(
@@ -135,7 +247,8 @@ def validate_finetuned_policy(
         vla=vla,
         action_tokenizer=action_tokenizer,
         dataloader=dataloader,
-        device_name=device_name
+        device_name=device_name,
+        processor=processor
     )
 
 
@@ -176,15 +289,6 @@ def validate_finetuned_policy_training_way(
     else:
         vla = vla.to(device_name)
 
-    norm_stats_json = os.path.join(checkpoint_path, "dataset_statistics.json")
-    with open(norm_stats_json, "r") as f:
-        norm_stats = json.load(f)
-
-    if len(norm_stats) != 1:
-        raise ValueError(f"Expected one dataset key, got {len(norm_stats)}")
-
-    vla.norm_stats = norm_stats  # hacky
-
     # Load dataset
     action_tokenizer = ActionTokenizer(processor.tokenizer)
     batch_transform = RLDSBatchTransform(
@@ -219,9 +323,10 @@ def validate_finetuned_policy_training_way(
         vla=vla,
         action_tokenizer=action_tokenizer,
         dataloader=dataloader,
-        device_name=device_name
+        device_name=device_name,
+        processor=processor
     )
 
 
 if __name__ == '__main__':
-    validate_finetuned_policy_training_way(use_quantization=False)
+    validate_finetuned_policy()
